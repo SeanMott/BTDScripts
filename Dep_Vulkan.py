4339a71112c8d4f8126feccf1438717d04cff674
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2024-08-20 08:44:37 -0400

    updated to add source files

--- Dep_Vulkan.py
+++ Dep_Vulkan.py
@@ -8,13 +8,20 @@ VMA_INCLUDE_DIR = "Venders/VMA/include"
 #gets the include directory
 VK_BOOTSTRAP_INCLUDE_DIR = "Venders/VKBootstrap/src"
 
+#lists all the files we need to include
+VK_BOOTSTRAP_SOURCE_FILE_NAMES = [
+	"VkBootstrap.cpp",
+	"VkBootstrap.h",
+	"VkBootstrapDispatch.h"
+]
+
+#adds the source paths to the source list in a Premake project
+def AddSources_VkBootstrap(srcs):
+	for f in VK_BOOTSTRAP_SOURCE_FILE_NAMES:
+		srcs.append(VK_BOOTSTRAP_INCLUDE_DIR + "/" + f)
+
 #the path to the vulkan include
 VULKAN_INCLUDE_DIR = "C:/VulkanSDK/1.3.283.0/Include"
-
-#gets the source files
-VK_BOOTSTRAP_SRC_DIR = """---vulkan bootstrapper
-    "Venders/VKBootstrap/src/**.cpp",
-    "Venders/VKBootstrap/src/**.h",\n\n"""
 
 #the link path for the vulkan
 VULKAN_LINK_DIR = "C:/VulkanSDK/1.3.283.0/Lib/vulkan-1.lib"
